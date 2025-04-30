098b32dc2a234bfd57faf457c96d0b678d6d3479
cesgarc <cesgarc@gmail.com>
2025-04-30 18:55:31 -0500

    Create products.py
    Módulo de productos desde Github

---
+++ products.py
@@ -0,0 +1,5 @@
+### Este sería un ejemplo de un nuevo archivo desde Github
+
+print('Creando el módulo de productos')
+
+print('Estamos interesados en Crear, Modificar, Consultar y eliminar Productos.')
